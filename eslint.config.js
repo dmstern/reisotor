3ecb700ecf390dbe534563a6f20e850d47e16d7e
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2026-08-31 08:11:07 +0200

    chore: Ignore storybook and claude for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -22,6 +22,8 @@ export default tseslint.config(
       '**/test-results/**',
       '**/coverage/**',
       '**/*.sqlite*',
+      '**/storybook-static/**',
+      '.claude',
     ],
   },
   ...tseslint.configs.recommended,
